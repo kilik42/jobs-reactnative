1708444e58647d05750d087735014cd0fde8a462
marvin evins <marvinevins@yahoo.com>
2023-04-11 16:40:40 -0500

    working on index section

--- app/index.js
+++ app/index.js
@@ -5,10 +5,27 @@ import {Stack, useRouter} from 'expo-router';
 import {Colors, icons, images, SIZES} from '../constants';
 import {Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome} from '../components';
 const Home = () => {
+    const router = useRouter();
+
     return (
-        <View>
-        <Text>Home</Text>
-        </View>
+        <SafeAreaView style={{
+            flex: 1,
+            backgroundColor: Colors.lightWhite,
+            paddingTop: SIZES.padding * 2,
+        }}>
+             
+             <Stack.Screen 
+             options={{
+                headerStyle: {
+                    backgroundColor: Colors.lightWhite},
+
+             }}
+            //  name="Home" 
+            //  component={Home} 
+             />
+
+
+        </SafeAreaView>
     );
     };
 
